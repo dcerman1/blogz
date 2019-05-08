a3d5931c3580619490017bb6f4875614369b09a3
Denise Cermanski <denise.cermanski@gmail.com>
2019-05-08 15:28:09 -0500

    Added base.html

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(15000))
+    publish = db.Column(db.Boolean())
 
     def __init__(self,title):
         self.title = title
@@ -18,24 +19,23 @@ class Blog(db.Model):
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-
     if request.method == 'POST':
         post_name = request.form['post']
         new_post = Blog(post_name)
         db.session.add(new_post)
         db.session.commit()
 
-    posts = Blog.query.filter_by(completed=False).all()
-    completed_tasks = Task.query.filter_by(completed=True).all()
+    posts = Blog.query.filter_by(publish=True).all()
+    published_posts = Blog.query.filter_by(publish=True).all()
     
-    return render_template('todos.html',title="Get It Done!", 
-    tasks=tasks, completed_tasks=completed_tasks)
+    return render_template('blogposts.html',title="My Blog!", 
+    posts=posts, published_posts=published_posts)
 
-@app.route('/delete-task', methods=['POST'])
+@app.route('/delete-blog', methods=['POST'])
 def delete_task():
-    task_id = int(request.form['task-id'])
-    task = Task.query.get(task_id)
-    task.completed = True
+    blog_id = int(request.form['blog-id'])
+    blog = Blog.query.get(blog_id)
+    blog.publish = True
     db.session.add(task)
     db.session.commit()
 

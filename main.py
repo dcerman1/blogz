b209b2326864bd6d6986c0bb044b64f2f8f412fc
Denise Cermanski <denise.cermanski@gmail.com>
2019-05-09 11:29:57 -0500

    BUG: post title and text not appearing on homepage

--- main.py
+++ main.py
@@ -10,36 +10,32 @@ db = SQLAlchemy(app)
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
-    body = db.Column(db.String(15000))
-    publish = db.Column(db.Boolean())
+    post = db.Column(db.String(15000))
+    publish = db.Column(db.Boolean)
 
-    def __init__(self,title):
+    def __init__(self, title, post):
         self.title = title
-        self.body = body
+        self.post = post
+        self.publish = False
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        post_name = request.form['post']
-        new_post = Blog(post_name)
-        db.session.add(new_post)
+        new_title = request.form['title']
+        new_post = request.form['post']
+        publish = True
+        new_blog = Blog(new_title, new_post)
+   
+        db.session.add(new_blog)
         db.session.commit()
-
-    posts = Blog.query.filter_by(publish=True).all()
-    published_posts = Blog.query.filter_by(publish=True).all()
     
-    return render_template('blogposts.html',title="My Blog!", 
-    posts=posts, published_posts=published_posts)
-
-@app.route('/delete-blog', methods=['POST'])
-def delete_task():
-    blog_id = int(request.form['blog-id'])
-    blog = Blog.query.get(blog_id)
-    blog.publish = True
-    db.session.add(task)
-    db.session.commit()
-
-    return redirect ('/')
-
+    posts = Blog.query.filter_by(publish=False).all()
+    return render_template('mainblog.html',title="MY BLOG", 
+        posts=posts)
+    
 if __name__ == '__main__':
     app.run()
+
+"""       blog_id = int(request.form['blog-id'])
+        blog = Task.query.get(blog_id)
+        blog.publish = True"""

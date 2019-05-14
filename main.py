5aa79a7bea8f8f4f304fff1f58a0b0a777327ff1
Denise Cermanski <denise.cermanski@gmail.com>
2019-05-14 16:57:21 -0500

    Added Login Page with Errors

--- main.py
+++ main.py
@@ -1,22 +1,54 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, flash, session
 from flask_sqlalchemy import SQLAlchemy
+from hashutils import make_pw_hash, check_pw_hash
+import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://build-a-blog:doitnow@localhost:8889/build-a-blog'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://blogz:password@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'y337kGcys&zP3B'
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     post = db.Column(db.String(15000))
     publish = db.Column(db.Boolean)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, post, publish):
+    def __init__(self, title, post, publish, owner):
         self.title = title
         self.post = post
         self.publish = publish
+        self.owner = owner
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120))
+    pw_hash = db.Column(db.String(15000))
+    posts = db.relationship('Blog', backref='owner')
+    
+    def __init__(self, username, password):
+        self.username = username
+        self.pw_hash = make_pw_hash(password)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_pw_hash(password, user.pw_hash):
+            session['username'] = username
+            flash("Logged in")
+            return redirect('/addpost')
+        else:
+            flash('Username Does Not Exist', 'error')
+            return redirect("/login")
+
 
 @app.route('/', methods=['GET'])
 def index():   
@@ -34,6 +66,7 @@ def view_post():
 
 @app.route('/addpost', methods=['POST', 'GET'])
 def add_post():
+    #new parameter(owner_id) consider when creating a blog entry.
     if request.method == 'POST':
         new_title = request.form['title']
         new_post = request.form['post']
